5024a449f9c7b97476faeab132fce27d224acb8f
gurdipgill <P16220697@my365.dmu.ac.uk>
2017-11-28 16:38:50 +0000

    First change
    Added CSS,HTML and js
    
    Responsive nav bar obtained from W3Schools

---
+++ README.md
@@ -0,0 +1,34 @@
+# A simple introduction to JavaScript and the DOM
+
+## Clone the lab files with GIT as follows
+
+If you have GIT available, *clone* this repository:
+
+- open the folder where you want to work, *don't* create a new folder for the exercise
+- open a command-line terminal to that folder
+- type `git clone https://github.com/CTEC3905/03-lab-javascript-1.git`
+- check in your folder, you should see a new folder "03-lab-javascript-1"
+- move into that folder with `cd 03-lab-javascript-1`
+- type `git remote -v` and you will see the URL above, which needs changing…
+
+## Set up your own repository for these files
+
+First, create *your own repository on GitHub* for these files, then change the GIT remote to the GitHub URL for your new repository with the following commands:
+
+- `git remote set-url origin [YOUR_REPO_URL_HERE]`
+
+then check your remote is your own repository with:
+
+- `git remote -v`
+
+## Key learning points for this exercise
+
+This exercise introduces a few fundamental Javascript principles:
+
+- **immediately-invoked function expressions (IIFE)** (stay out of the global namespace)
+- **event listeners** (for capturing user actions)
+- **DOM manipulation** (for dynamically changing HTML elements and/or their properties)
+
+Read through the comments in the "greetings-comments.js" code and try out the suggestions in "greetings.js".
+
+You can [view this page here](https://ctec3905.github.io/03-lab-javascript-1/).
---
+++ js/greeting-comments.js
@@ -0,0 +1,47 @@
+(function(){
+/*
+  An "Immediately-Invoked Function Expression" (IIFE) contains
+  all the code. the final brackets () ensure that it executes.
+
+  This is good practice because:
+  - it keeps your variables out of the global namespace
+  - local references to objects decrease lookup times
+  - it is a foundation for further code organisation
+
+  It is also an "anonymous" function because it has/needs no name.
+*/
+  
+  /*
+    declare all the variables using 'let'. These get a reference
+    to the HTML elements we want by finding their ID
+  */
+  let myName = document.getElementById("my-name"); 
+  let userName = document.getElementById("user-name"); 
+  let getName = document.getElementById("get-name");
+  
+  // changes the 'innerHTML' (text) of the element
+  function PerformGreeting(ev) {
+    myName.innerHTML = userName.value;
+    ev.preventDefault();
+  }
+  /*
+    Without setting the variables first, the above line would be really long:
+    document.getElementById("my-name").innerHTML = document.getElementById("user-name").value;
+  */
+
+  /*
+    Event Listeners keep your JavaScript out of the HTML -
+    professional developers avoid (e.g.) "onclick" in HTML tags.
+    This 'listens' for the user to submit the form.
+    You can attach multiple events to any single element.
+  */
+  getName.addEventListener("submit", PerformGreeting);
+
+  /*
+    Uncomment the line below - see what happens when you click
+    in the grey area outside the input "submit" button. Why?
+  */
+  
+  // getName.addEventListener("click", PerformGreeting);
+
+}());

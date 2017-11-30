ca284710d4f3d2a9995efa178770377874010b87
gurdipgill <P16220697@my365.dmu.ac.uk>
2017-11-30 10:26:34 +0000

    Timeline
    time line added next job is to sort the errors on the html out

--- js/greeting-comments.js
+++ js/greeting-comments.js
@@ -1,47 +1,121 @@
-(function(){
-/*
-  An "Immediately-Invoked Function Expression" (IIFE) contains
-  all the code. the final brackets () ensure that it executes.
+(function() {
 
-  This is good practice because:
-  - it keeps your variables out of the global namespace
-  - local references to objects decrease lookup times
-  - it is a foundation for further code organisation
+  // VARIABLES
+  const timeline = document.querySelector(".timeline ol"),
+    elH = document.querySelectorAll(".timeline li > div"),
+    arrows = document.querySelectorAll(".timeline .arrows .arrow"),
+    arrowPrev = document.querySelector(".timeline .arrows .arrow__prev"),
+    arrowNext = document.querySelector(".timeline .arrows .arrow__next"),
+    firstItem = document.querySelector(".timeline li:first-child"),
+    lastItem = document.querySelector(".timeline li:last-child"),
+    xScrolling = 280,
+    disabledClass = "disabled";
 
-  It is also an "anonymous" function because it has/needs no name.
-*/
-  
-  /*
-    declare all the variables using 'let'. These get a reference
-    to the HTML elements we want by finding their ID
-  */
-  let myName = document.getElementById("my-name"); 
-  let userName = document.getElementById("user-name"); 
-  let getName = document.getElementById("get-name");
-  
-  // changes the 'innerHTML' (text) of the element
-  function PerformGreeting(ev) {
-    myName.innerHTML = userName.value;
-    ev.preventDefault();
+  // START
+  window.addEventListener("load", init);
+
+  function init() {
+    setEqualHeights(elH);
+    animateTl(xScrolling, arrows, timeline);
+    setSwipeFn(timeline, arrowPrev, arrowNext);
+    setKeyboardFn(arrowPrev, arrowNext);
   }
-  /*
-    Without setting the variables first, the above line would be really long:
-    document.getElementById("my-name").innerHTML = document.getElementById("user-name").value;
-  */
 
-  /*
-    Event Listeners keep your JavaScript out of the HTML -
-    professional developers avoid (e.g.) "onclick" in HTML tags.
-    This 'listens' for the user to submit the form.
-    You can attach multiple events to any single element.
-  */
-  getName.addEventListener("submit", PerformGreeting);
+  // SET EQUAL HEIGHTS
+  function setEqualHeights(el) {
+    let counter = 0;
+    for (let i = 0; i < el.length; i++) {
+      const singleHeight = el[i].offsetHeight;
 
-  /*
-    Uncomment the line below - see what happens when you click
-    in the grey area outside the input "submit" button. Why?
-  */
-  
-  // getName.addEventListener("click", PerformGreeting);
+      if (counter < singleHeight) {
+        counter = singleHeight;
+      }
+    }
 
-}());
+    for (let i = 0; i < el.length; i++) {
+      el[i].style.height = `${counter}px`;
+    }
+  }
+
+  // CHECK IF AN ELEMENT IS IN VIEWPORT
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // SET STATE OF PREV/NEXT ARROWS
+  function setBtnState(el, flag = true) {
+    if (flag) {
+      el.classList.add(disabledClass);
+    } else {
+      if (el.classList.contains(disabledClass)) {
+        el.classList.remove(disabledClass);
+      }
+      el.disabled = false;
+    }
+  }
+
+  // ANIMATE TIMELINE
+  function animateTl(scrolling, el, tl) {
+    let counter = 0;
+    for (let i = 0; i < el.length; i++) {
+      el[i].addEventListener("click", function() {
+        if (!arrowPrev.disabled) {
+          arrowPrev.disabled = true;
+        }
+        if (!arrowNext.disabled) {
+          arrowNext.disabled = true;
+        }
+        const sign = (this.classList.contains("arrow__prev")) ? "" : "-";
+        if (counter === 0) {
+          tl.style.transform = `translateX(-${scrolling}px)`;
+        } else {
+          const tlStyle = getComputedStyle(tl);
+          // add more browser prefixes if needed here
+          const tlTransform = tlStyle.getPropertyValue("-webkit-transform") || tlStyle.getPropertyValue("transform");
+          const values = parseInt(tlTransform.split(",")[4]) + parseInt(`${sign}${scrolling}`);
+          tl.style.transform = `translateX(${values}px)`;
+        }
+
+        setTimeout(() => {
+          isElementInViewport(firstItem) ? setBtnState(arrowPrev) : setBtnState(arrowPrev, false);
+          isElementInViewport(lastItem) ? setBtnState(arrowNext) : setBtnState(arrowNext, false);
+        }, 1100);
+
+        counter++;
+      });
+    }
+  }
+
+  // ADD SWIPE SUPPORT FOR TOUCH DEVICES
+  function setSwipeFn(tl, prev, next) {
+    const hammer = new Hammer(tl);
+    hammer.on("swipeleft", () => next.click());
+    hammer.on("swiperight", () => prev.click());
+  }
+
+  // ADD BASIC KEYBOARD FUNCTIONALITY
+  function setKeyboardFn(prev, next) {
+    document.addEventListener("keydown", (e) => {
+      if ((e.which === 37) || (e.which === 39)) {
+        const timelineOfTop = timeline.offsetTop;
+        const y = window.pageYOffset;
+        if (timelineOfTop !== y) {
+          window.scrollTo(0, timelineOfTop);
+        }
+        if (e.which === 37) {
+          prev.click();
+        } else if (e.which === 39) {
+          next.click();
+        }
+      }
+    });
+  }
+
+})();
